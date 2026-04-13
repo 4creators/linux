75e82a51c2ec99daae7a0386162fd273197522f6
Jacek Blaszczynski <jacek.blaszczynski@outlook.com>
2026-04-13 08:10:16 +0200

    hv: dxgkrnl: Add Microsoft Paravirtualized GPU support
    Port the Microsoft Paravirtualized GPU support driver (dxgkrnl) from
    the WSL2 Linux kernel (tag linux-msft-wsl-6.18.20.1) to the mainline
    kernel.
    
    This driver supports paravirtualized virtual compute devices exposed
    by Microsoft Hyper-V when Linux is running inside of a virtual machine
    hosted by Windows.
    
    The following modifications were made for upstream integration:
    - Hooked dxgkrnl into drivers/hv/Kconfig and drivers/hv/Makefile.
    - Added DXGK-related GUID definitions (HV_GPUP_DXGK_GLOBAL_GUID and
      HV_GPUP_DXGK_VGPU_GUID) to include/linux/hyperv.h.
    - Updated dxgdmafence_signaled() in dxgsyncfile.c to use the updated
      signature for __dma_fence_is_later().
    
    Developed with the assistance of Gemini.
    
    Signed-off-by: Jacek Blaszczynski <jacek.blaszczynski@outlook.com>

--- include/linux/hyperv.h
+++ include/linux/hyperv.h
@@ -1441,6 +1441,22 @@ void vmbus_free_mmio(resource_size_t start, resource_size_t size);
 			  0x6f, 0xd0, 0xbe, 0x52, 0x8c, 0xda)
 
 /*
+ * GPU paravirtualization global DXGK channel
+ * {DDE9CBC0-5060-4436-9448-EA1254A5D177}
+ */
+#define HV_GPUP_DXGK_GLOBAL_GUID \
+	.guid = GUID_INIT(0xdde9cbc0, 0x5060, 0x4436, 0x94, 0x48, \
+			  0xea, 0x12, 0x54, 0xa5, 0xd1, 0x77)
+
+/*
+ * GPU paravirtualization per virtual GPU DXGK channel
+ * {6E382D18-3336-4F4B-ACC4-2B7703D4DF4A}
+ */
+#define HV_GPUP_DXGK_VGPU_GUID \
+	.guid = GUID_INIT(0x6e382d18, 0x3336, 0x4f4b, 0xac, 0xc4, \
+			  0x2b, 0x77, 0x3, 0xd4, 0xdf, 0x4a)
+
+/*
  * Guest File Copy Service
  * {34D14BE3-DEE4-41c8-9AE7-6B174977C192}
  */
